de6b2dc8723cace830edb41e9a9d47585d82096f
Justin Oakes <Justin.Oakes@loves.com>
2019-04-23 13:26:29 -0500

    keychain migration method added

--- Auth0/CredentialsManager.swift
+++ Auth0/CredentialsManager.swift
@@ -168,6 +168,9 @@ public struct CredentialsManager {
 
 extension CredentialsManager {
     func migrateKeychain(from oldKeychain: A0SimpleKeychain, to newKeychain: A0SimpleKeychain) {
-        
+        if let credentials = oldKeychain.data(forKey: storeKey) {
+            newKeychain.setData(credentials, forKey: storeKey)
+            oldKeychain.deleteEntry(forKey: storeKey)
+        }
     }
 }
